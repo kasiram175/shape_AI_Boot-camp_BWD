9052372befd3aab65f93412290c5952b4dc4ed35
kasiram175 <85451213+kasiram175@users.noreply.github.com>
2021-06-06 22:08:32 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1> ShapeAI With sinha-sir</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> Javascript and React.js</h1>
+      <p>
+        This was an amazing bootcamp with sinha sir and we learned alot of thing
+        from him about ract.js and javascripts ,we expect a lot of information
+        from him....
+      </p>
+    </div>
+  );
+}
+
+export default Note;
